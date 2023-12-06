6bad7f3b5ecea4f06c2ee639e9daa7ec90c9d296
alexngumbau <111250066+alexngumbau@users.noreply.github.com>
2023-12-06 10:21:10 +0300

    Exposing the entit IDS

--- E-Commerce-BE/src/main/java/com/example/springbootecommerce/dao/ProductCategoryRepository.java
+++ E-Commerce-BE/src/main/java/com/example/springbootecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,6 @@ package com.example.springbootecommerce.dao;
 import com.example.springbootecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
