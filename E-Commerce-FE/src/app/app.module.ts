2466055fb0a6f677221b2093c248a3f1ed7606a0
alexngumbau <111250066+alexngumbau@users.noreply.github.com>
2023-12-06 17:57:46 +0300

    Send data to search route. Enhanced product list component, updated product service api, handled for no products found.

--- E-Commerce-FE/src/app/app.module.ts
+++ E-Commerce-FE/src/app/app.module.ts
@@ -3,24 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './componets/product-list/product-list.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 const routes : Routes = [
-  {path : 'category/:id' , component : ProductListComponent},
+  {path : 'search/:keyword', component: ProductListComponent},
+  {path : 'category/:id/:name' , component : ProductListComponent},
   {path : 'category' , component : ProductListComponent},
   {path : 'products' , component : ProductListComponent},
   {path : '', redirectTo: '/products' , pathMatch : 'full'},
   {path : '**' , redirectTo: '/products', pathMatch : 'full'}
+  
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
+    ProductCategoryMenuComponent,
+    SearchComponent,
     
   ],
   imports: [
--- E-Commerce-FE/src/app/services/product.service.ts
+++ E-Commerce-FE/src/app/services/product.service.ts
@@ -2,28 +2,55 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+ 
 
   private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product-category';
   constructor(private httpClient : HttpClient) { }
 
   getProductList(theCategoryId : number) : Observable<Product[] > {
   // need to build URL based on category id
   const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    return this.getProducts(searchUrl)
+  }
 
 
-    return this.httpClient.get<GetResponse> (searchUrl).pipe(
+  getProductCategories() : Observable<ProductCategory[] > {
+
+    return this.httpClient.get<GetResponseProductsCategory> (this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+    
+  }
+
+  searchProducts(theKeyword : string): Observable<Product[]> {
+    // need to buuild URL based on the keyword
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+    return this.getProducts(searchUrl);
+  } 
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
+
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products : Product[];
+  }
+}
+
+interface GetResponseProductsCategory {
+  _embedded: {
+    productCategory : ProductCategory[];
   }
 }
