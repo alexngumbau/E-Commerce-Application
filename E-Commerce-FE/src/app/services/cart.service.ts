08368c18ec3c8ea7206b74389f49b99139b0c723
alexngumbau <111250066+alexngumbau@users.noreply.github.com>
2023-12-12 19:17:25 +0300

    Added the increment, decrement and remove button in the cart

--- E-Commerce-FE/src/app/services/cart.service.ts
+++ E-Commerce-FE/src/app/services/cart.service.ts
@@ -6,6 +6,7 @@ import { Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class CartService {
+  
 
   cartItems: CartItem[] = [];
   totalPrice : Subject<number> = new Subject<number>();
@@ -78,6 +79,27 @@ export class CartService {
     
     
   }
+
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity--;
+    if (theCartItem.quantity == 0) {
+      this.remove(theCartItem);
+    } else{
+      this.computeCartTotals();
+    }
+  }
+
+  remove(theCartItem: CartItem) {
+    // get index of item in the array
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id === theCartItem.id);
+    // if found, remove the item from the array of the given index
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+
+      this.computeCartTotals();
+    }
+  }
+  
 
    
 }
