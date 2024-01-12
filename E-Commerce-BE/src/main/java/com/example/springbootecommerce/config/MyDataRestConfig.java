2b57ffb0b0f0cbde6e72dd5c9b2927cee6f06121
alexngumbau <111250066+alexngumbau@users.noreply.github.com>
2024-01-12 10:50:53 +0300

    Configured cors mapping

--- E-Commerce-BE/src/main/java/com/example/springbootecommerce/config/MyDataRestConfig.java
+++ E-Commerce-BE/src/main/java/com/example/springbootecommerce/config/MyDataRestConfig.java
@@ -7,6 +7,7 @@ import com.example.springbootecommerce.entity.State;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -19,6 +20,9 @@ import java.util.Set;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Value("${allowed.origins}")
+    private String[] theAllowedOrigins;
 
     private EntityManager entityManager;
     @Autowired
@@ -39,7 +43,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         exposeIds(config);
 
         // Configure the cors mapping
-        cors.addMapping("/api/**").allowedOrigins("http://localhost:4200");
+        cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
     }
 
     private static void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnSupportedActions) {
