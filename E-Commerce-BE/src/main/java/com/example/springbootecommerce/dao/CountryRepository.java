3cb9bf3c5eb13f4f850ad5d4a3b673cf54a003d0
alexngumbau <111250066+alexngumbau@users.noreply.github.com>
2024-01-20 17:00:33 +0300

    Configured spring boot to use a self-signed certificate for https

--- E-Commerce-BE/src/main/java/com/example/springbootecommerce/dao/CountryRepository.java
+++ E-Commerce-BE/src/main/java/com/example/springbootecommerce/dao/CountryRepository.java
@@ -3,8 +3,6 @@ package com.example.springbootecommerce.dao;
 import com.example.springbootecommerce.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
 
 @RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
 public interface CountryRepository extends JpaRepository<Country, Integer> {
