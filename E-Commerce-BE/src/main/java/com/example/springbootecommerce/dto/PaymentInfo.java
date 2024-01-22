69b70b298e4485abf9895261ccf7ffeabea2a60d
alexngumbau <111250066+alexngumbau@users.noreply.github.com>
2024-01-22 03:03:13 +0300

    Created a method for creating payment Intent -- Stripe Integration process

---
+++ E-Commerce-BE/src/main/java/com/example/springbootecommerce/dto/PaymentInfo.java
@@ -0,0 +1,9 @@
+package com.example.springbootecommerce.dto;
+
+import lombok.Data;
+
+@Data
+public class PaymentInfo {
+    private int amount;
+    private String currency;
+}
--- E-Commerce-BE/src/main/java/com/example/springbootecommerce/service/CheckoutService.java
+++ E-Commerce-BE/src/main/java/com/example/springbootecommerce/service/CheckoutService.java
@@ -1,8 +1,13 @@
 package com.example.springbootecommerce.service;
 
+import com.example.springbootecommerce.dto.PaymentInfo;
 import com.example.springbootecommerce.dto.Purchase;
 import com.example.springbootecommerce.dto.PurchaseResponse;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 public interface CheckoutService {
     PurchaseResponse placeOrder(Purchase purchase);
+
+    PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException;
 }
